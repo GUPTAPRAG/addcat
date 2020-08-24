56adef3c82604d37d2928982d8ce768bab900e3a
GUPTAPRAG <34096368+GUPTAPRAG@users.noreply.github.com>
2020-08-24 16:00:05 +0100

    Add files via upload

---
+++ Category.php
@@ -0,0 +1,42 @@
+﻿<?php 
+ include_once '../lib/database.php';
+include_once '../helpers/format.php';
+
+?>
+
+<?php
+
+class Category{
+	private $db;
+	private $fm;
+public function __construct(){
+	$this->db = new Database();
+	$this->fm = new Format();
+	
+}
+	 public function catInsert($catName){                
+    $catName = $this->fm->validation($catName); // Validation for special Characters             
+    $catName =  mysqli_real_escape_string($this->db->link, $catName ); // Validation for mysqli   
+    if (empty($catName)) {
+    	 $msg = "Category Field must not be empty"; // validation for empty 
+    	 return $msg;
+    	}else {
+			$query = "INSERT INTO tbl_category(catName) VALUES ('$catName')";  
+    		$catinsert = $this->db->insert($query);
+    		if ($catinsert) {
+     $msg = "<span class='success'>Category Inserted Successfully.</span> "; // I create one span class
+         return $msg; // Here i return this Message 
+    		}else {
+     $msg = "<span class='error'>Category Not Inserted .</span> "; // I create one span class as error
+    	 return $msg; // Here i return this Message 
+    		}
+
+			}
+			
+		}
+
+	}
+	
+
+
+?>
---
+++ catadd.php
@@ -0,0 +1,45 @@
+﻿<?php include 'inc/header.php';?>
+<?php include 'inc/sidebar.php';?>
+<?php include '../classes/Category.php';?>
+
+<?php
+   $cat = new Category();
+   if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $catName = $_POST['catName']; // here i add our input filed name 
+        $insertCat = $cat->catInsert($catName); // with this Category object i access one method. 
+    }
+
+
+
+
+
+?>
+
+        <div class="grid_10">
+            <div class="box round first grid">
+                <h2>Add New Category</h2>
+               <div class="block copyblock"> 
+
+               <?php
+ if (isset($insertCat)) { // showing this return $msg
+ echo $insertCat;
+    }
+  ?>
+                 <form action=" " method="post">
+                    <table class="form">					
+                        <tr>
+                            <td>
+                                <input type="text" name="catName" class="medium" />
+                            </td>
+                        </tr>
+						<tr> 
+                            <td>
+                                <input type="submit" name="submit" Value="Save" />
+                            </td>
+                        </tr>
+                    </table>
+                    </form>
+                </div>
+            </div>
+        </div>
+<?php include 'inc/footer.php';?>
